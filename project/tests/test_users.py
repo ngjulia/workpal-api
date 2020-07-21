e57eb75d74ef0e9514b1b9bdf0dba9c4d66db452
ngjulia <jen67@cornell.edu>
2020-07-21 03:30:28 -0400

    create task tests

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -29,3 +29,50 @@ def test_create_user_invalid_json(test_app):
             }
         ]
     }
+
+def test_create_task(test_app_with_db):
+  response = test_app_with_db.post(
+    "/user/", data=json.dumps({"full_name": "Kevin", "email": "test@gmail.com","phone": "123-123-1234"})
+  )
+  user_id = response.json()["id"]
+  response = test_app_with_db.post(
+    f"/user/{user_id}/task/", data=json.dumps({"name": "Learn to code", "rank": 1, "completed": False, "completion_time": 0 })
+  )
+  assert response.status_code == 201
+  assert response.json()["name"] == "Learn to code"
+  assert response.json()["rank"] == 1
+  assert response.json()["completed"] == False
+  assert response.json()["completion_time"] == 0
+  assert response.json()["user_id"] == user_id
+
+def test_create_task_invalid_json(test_app_with_db):
+    response = test_app_with_db.post(
+      "/user/", data=json.dumps({"full_name": "Kevin", "email": "test@gmail.com","phone": "123-123-1234"})
+    )
+    user_id = response.json()["id"]
+    response = test_app_with_db.post(f"/user/{user_id}/task/", data=json.dumps({}))
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "loc": ["body", "payload", "name"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            },
+            {
+                "loc": ["body", "payload", "rank"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            },
+            {
+                "loc": ["body", "payload", "completed"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            },
+            {
+                "loc": ["body", "payload", "completion_time"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            }
+        ]
+    }
